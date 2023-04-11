bbbd3d826931e66c0edb2a943f951d97d147d959
Dmitry <k1791@mail.ru>
2023-04-11 23:06:14 +0500

    step5/3 lesson (1)

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,6 +2,12 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+select  Customers.company_name,  CONCAT(Employees.first_name, ' ', Employees.last_name) as ФИО,  Shippers.company_name
+from orders Join customers USING (customer_id)
+Join employees USING (employee_id)
+Join shippers on  Orders.ship_via = Shippers.shipper_id
+where Customers.city ='London' and Employees.city ='London' and Shippers.company_name  = 'United Package'
+
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
